f885d2df208b43ba856e7524cc231360334ee5b1
eric556 <enpie123@gmail.com>
2016-03-31 14:48:52 -0500

    Starting Event Manager

---
+++ SFGE_Core/EventManager.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <SFML\Graphics.hpp>
+
+enum class EventType{
+	KeyDown = sf::Event::KeyPressed,
+	KeyUp = sf::Event::KeyReleased,
+	MButtonDown = sf::Event::MouseButtonPressed,
+	MButtonUp = sf::Event::MouseButtonReleased,
+	MouseWheel = sf::Event::MouseWheelMoved,
+	WindowResized = sf::Event::Resized,
+	GainedFocus = sf::Event::LostFocus,
+	MouseEntered = sf::Event::MouseEntered,
+	MouseLeft = sf::Event::MouseLeft,
+	Closed = sf::Event::Closed,
+	TextEntered = sf::Event::TextEntered,
+	Keyboard = sf::Event::Count + 1, Mouse, Joystick
+};
+
+struct EventInfo{
+
+};
