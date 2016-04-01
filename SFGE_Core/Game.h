37ece822676db064a3e2e16a87a01b39fd6df11d
eric556 <enpie123@gmail.com>
2016-04-01 09:10:24 -0500

    Adding to the event manager

--- SFGE_Core/Game.h
+++ SFGE_Core/Game.h
@@ -2,22 +2,24 @@
 #include "Window.h"
 #include <SFML\Graphics.hpp>
 
-class Game{
-public:
-	
+namespace SFGE{
+	class Game{
+	public:
 
-	virtual void HandleInput() = 0;
-	virtual void Render() = 0;
-	virtual void Update() = 0;
 
-	Window* GetWindow(){
-		return &m_window;
-	}
+		virtual void HandleInput() = 0;
+		virtual void Render() = 0;
+		virtual void Update() = 0;
 
-	sf::Time GetElapsed(){ return m_clock.getElapsedTime(); }
-	void RestartClock(){ m_elapsed = m_clock.restart().asSeconds(); }
-protected:
-	Window m_window;
-	sf::Clock m_clock;
-	float m_elapsed;
-};
+		Window* GetWindow(){
+			return &m_window;
+		}
+
+		sf::Time GetElapsed(){ return m_clock.getElapsedTime(); }
+		void RestartClock(){ m_elapsed = m_clock.restart().asSeconds(); }
+	protected:
+		Window m_window;
+		sf::Clock m_clock;
+		float m_elapsed;
+	};
+}
